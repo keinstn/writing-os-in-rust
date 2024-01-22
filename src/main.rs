baa24d57849925922594bafd77fe598b51a1ef0b
keinstn <kei.sj.nstn@gmail.com>
2024-01-22 21:26:49 +0900

    Added a global interface for write. Added println macro.

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello world{}", "!");
 
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+
     loop {}
 }
