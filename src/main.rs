bfb12b93379e88c7b447fa426c4673f65af71190
keinstn <kei.sj.nstn@gmail.com>
2024-01-28 16:10:25 +0900

    Support page fault exception

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,14 @@ pub extern "C" fn _start() -> ! {
 
     // x86_64::instructions::interrupts::int3();
 
+    use x86_64::registers::control::Cr3;
+
+    let (level4_page_table, _) = Cr3::read();
+    println!(
+        "Level 4 page table at: {:?}",
+        level4_page_table.start_address()
+    );
+
     println!("It did not crash!");
     my_os::hlt_loop();
 }
