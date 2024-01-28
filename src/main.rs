b42fbe4e5125d3358e1f40e932538d5bf6bbbd5b
keinstn <kei.sj.nstn@gmail.com>
2024-01-28 20:29:45 +0900

    Translate virtual address to physical address

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,58 @@
 #![no_std]
 #![no_main]
 
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
 use my_os::println;
 
-use core::panic::PanicInfo;
+entry_point!(kernel_main);
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use my_os::memory::{active_level_4_table, translate_addr};
+    use x86_64::structures::paging::PageTable;
+    use x86_64::VirtAddr;
+
     println!("Hello world{}", "!");
-
     my_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
+
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 Entry {}: {:?}", i, entry);
+
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //         let ptr = VirtAddr::new(virt).as_mut_ptr();
+    //         let l3_table: &PageTable = unsafe { &*ptr };
+
+    //         for (i, entry) in l3_table.iter().enumerate() {
+    //             if !entry.is_unused() {
+    //                 println!("L3 Entry {}: {:?}", i, entry);
+    //             }
+    //         }
+    //     }
+    // }
 
     // fn stack_overflow() {
     //     stack_overflow();
@@ -23,13 +66,13 @@ pub extern "C" fn _start() -> ! {
 
     // x86_64::instructions::interrupts::int3();
 
-    use x86_64::registers::control::Cr3;
-
-    let (level4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level4_page_table.start_address()
-    );
+    // use x86_64::registers::control::Cr3;
+    //
+    // let (level4_page_table, _) = Cr3::read();
+    // println!(
+    //     "Level 4 page table at: {:?}",
+    //     level4_page_table.start_address()
+    // );
 
     println!("It did not crash!");
     my_os::hlt_loop();
